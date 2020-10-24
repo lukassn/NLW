abe0dcef7dbbca71b64d0a3a88db176710313ab1
Lukas Nascimento <lukas.nascimento@ccc.ufcg.edu.br>
2020-10-24 11:05:19 -0300

    aula 05

---
+++ NLW/src/database/db.js
@@ -0,0 +1,9 @@
+const Database = require('sqlite3')
+Database.open(__dirname + '/database.sqlite').then(execute)
+
+function execute(db){
+  console.log("cheugeui aquiiiiiii")
+  //console.log(db)
+
+
+}
